2aa4398935ca8012641339de77b430a5e1af814e
luisbryandr <luisbdominguezr@gmail.com>
2021-10-25 23:47:31 -0400

    created initial central state for app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import TodoForm from './components/TodoForm';
 
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state= {
+      todos: [
+        {
+          task: "Organize Garage",
+          id: 3,
+          completed: false,
+        },
+        {
+          task: "Pay Bills",
+          id: 10,
+          completed: false,
+        },
+      ],
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -11,6 +28,7 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoForm />
+        <TodoList />
       </div>
     );
   }

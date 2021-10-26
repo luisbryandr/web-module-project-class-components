fa89ef692b160ed90a6ef60371e5864497b3fe76
luisbryandr <luisbdominguezr@gmail.com>
2021-10-25 23:49:52 -0400

    passing down state to Todo List component

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoForm />
-        <TodoList />
+        <TodoList todos={this.state.todos} /> 
       </div>
     );
   }
